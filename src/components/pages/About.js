d7bf7fecb051f1c89d557094134cd742c11978a3
Prazin Karki <pk@Prazins-MBP.modem>
2020-02-01 16:30:32 +1100

    Update About page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <>
             <h1>About this App</h1>
-            <p>App to search Github users</p>
+            <p>App to search Github users profile</p>
         </>
     )
 }
